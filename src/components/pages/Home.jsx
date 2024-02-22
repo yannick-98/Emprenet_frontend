61547598fff4865dbca7882b03c01c0255d9e405
yannick-98 <yannick9862@gmail.com>
2024-02-22 16:52:02 +0100

    ajustes en home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import Feed from '../posts/Feed'
-import CreatePost from '../posts/CreatePost'
 
 const Home = () => {
     return (
-        <section className=''>
-            <Feed />
-        </section>
+        <Feed />
     )
 }
 
